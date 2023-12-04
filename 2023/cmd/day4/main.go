3045dd669745584d053e9e36ea2eb31c05486b11
Justin Ray <github@biz.raybetter.com>
2023-12-04 09:39:06 -0500

    Day 4 solution

--- 2023/cmd/day4/main.go
+++ 2023/cmd/day4/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"aoc2023/internal/helpers"
+	h "aoc2023/internal/helpers"
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 )
 
 func main() {
 
 	d := &Day4{}
 
-	err := helpers.Dispatch(os.Args, d)
+	err := h.Dispatch(os.Args, d)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
@@ -19,17 +21,94 @@ func main() {
 }
 
 type Day4 struct {
+	lines []string
+	cards []Card
+}
+
+type Card struct {
+	ID      int
+	Copies  int
+	Winners []int
+	Numbers []int
 }
 
 func (d *Day4) Setup(filename string) {
 	fmt.Println("Setup")
+	d.lines = h.ReadFileToLines(filename)
+
+	for _, line := range d.lines {
+		cardTokens := strings.Split(line, ":")
+		h.Assert(len(cardTokens) == 2, "card_tokens not len 2")
+
+		card := Card{}
+
+		card.ID, _ = h.ParseLabeledValue(cardTokens[0], false)
+		numberListTokens := strings.Split(cardTokens[1], "|")
+		h.Assert(len(numberListTokens) == 2, "numberListTokens not len 2")
+		card.Winners = h.ParseIntList(strings.TrimSpace(numberListTokens[0]), " ")
+		card.Numbers = h.ParseIntList(strings.TrimSpace(numberListTokens[1]), " ")
+		card.Copies = 1
+
+		d.cards = append(d.cards, card)
+	}
+
 }
 
-
 func (d *Day4) Part1() {
-    fmt.Println("Part 1")
+	fmt.Println("Part 1")
+
+	score := 0
+
+	for _, card := range d.cards {
+		fmt.Printf("%#v\n", card)
+		cardScore := 0
+
+		for _, winner := range card.Winners {
+			if slices.Contains(card.Numbers, winner) {
+				//found a match
+				if cardScore == 0 {
+					cardScore = 1
+				} else {
+					cardScore = cardScore * 2
+				}
+			}
+		}
+
+		fmt.Println("  score=", cardScore)
+
+		score += cardScore
+	}
+	fmt.Println("Total score: ", score)
 }
 
 func (d *Day4) Part2() {
-    fmt.Println("Part 2")
+	fmt.Println("Part 1")
+
+	for cardIndex, card := range d.cards {
+		fmt.Printf("%#v\n", card)
+
+		matchCount := 0
+		for _, winner := range card.Winners {
+			if slices.Contains(card.Numbers, winner) {
+				//found a match
+				matchCount += 1
+			}
+		}
+
+		//incre``ment the copies for successive cards, depenting on matchs
+		for matchOffset := 1; matchOffset <= matchCount; matchOffset++ {
+			//we add a copy for successive cards for every copy of the current card
+			d.cards[cardIndex+matchOffset].Copies += card.Copies
+		}
+
+		fmt.Println("  matches=", matchCount)
+	}
+
+	//count the total number of cards
+	cardCount := 0
+	for _, card := range d.cards {
+		cardCount += card.Copies
+	}
+
+	fmt.Println("Total card count: ", cardCount)
 }
--- 2023/internal/helpers/textparse.go
+++ 2023/internal/helpers/textparse.go
@@ -6,12 +6,22 @@ import (
 	"strings"
 )
 
+func StripEmptyStrings(input []string) []string {
+	temp := []string{}
+	for _, token := range input {
+		if len(token) > 0 {
+			temp = append(temp, token)
+		}
+	}
+	return temp
+}
+
 // ParselabeledValue expects a string like "7 sheep" and returns 7 as an int and the string "sheep"
 // if numberFirst is true, it processes the string like "7 sheep"
 // if numberFirst is false, it processes the string liek "Game 12"
 // if the string does not conform to the pattern, then it panics
 func ParseLabeledValue(s string, numberFirst bool) (int, string) {
-	tokens := strings.Split(s, " ")
+	tokens := StripEmptyStrings(strings.Split(s, " "))
 	Assert(len(tokens) == 2, "token len != 2")
 
 	numIndex := 0
@@ -27,4 +37,24 @@ func ParseLabeledValue(s string, numberFirst bool) (int, string) {
 	}
 
 	return numVal, tokens[labelIndex]
+}
+
+func ParseIntList(s string, sep string) []int {
+	tokens := StripEmptyStrings(strings.Split(s, sep))
+
+	// for _, token := range tokens {
+	// 	fmt.Printf("'%s', ", token)
+	// }
+	// fmt.Println("")
+
+	numbers := make([]int, len(tokens))
+
+	var err error
+	for index, token := range tokens {
+		numbers[index], err = strconv.Atoi(token)
+		if err != nil {
+			panic(fmt.Errorf("could not convert '%s' to an int: %w", token, err))
+		}
+	}
+	return numbers
 }
