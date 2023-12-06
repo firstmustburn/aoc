9a7ab456b30d0a8465f509eeb8dc77cc81f64034
Justin Ray <github@biz.raybetter.com>
2023-12-05 19:17:18 -0500

    Add SumSlice to helpers

--- 2023/internal/helpers/util.go
+++ 2023/internal/helpers/util.go
@@ -1,5 +1,7 @@
 package helpers
 
+import "golang.org/x/exp/constraints"
+
 func Assert(condition bool, reason string) {
 	if !condition {
 		panic(reason)
@@ -8,4 +10,13 @@ func Assert(condition bool, reason string) {
 
 func IsDigit(c byte) bool {
 	return c >= '0' && c <= '9'
+}
+
+func SumSlice[V constraints.Integer | constraints.Float](input []V) V {
+	var theSum V
+
+	for _, i := range input {
+		theSum += i
+	}
+	return theSum
 }
