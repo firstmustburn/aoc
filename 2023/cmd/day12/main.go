5b06c7b46127f948c4117e55cc5a627666af8327
Justin Ray <github@biz.raybetter.com>
2023-12-12 22:44:43 -0500

    Day 12 solution

--- 2023/cmd/day12/main.go
+++ 2023/cmd/day12/main.go
@@ -32,9 +32,18 @@ func main() {
 	// programLevel.Set(slog.LevelInfo)
 	programLevel.Set(slog.LevelWarn)
 
+	var err error
+
+	// f, err := os.Create("Day12.prof")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// pprof.StartCPUProfile(f)
+	// defer pprof.StopCPUProfile()
+
 	d := &Day12{}
 
-	err := h.Dispatch(os.Args, d)
+	err = h.Dispatch(os.Args, d)
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
@@ -62,11 +71,15 @@ func (d *Day12) Setup(filename string) {
 
 }
 
-const SPRING_GOOD byte = '.'
-const SPRING_BAD byte = '#'
-const SPRING_UNKNOWN byte = '?'
+const SPRING_GOOD_C byte = '.'
+const SPRING_BAD_C byte = '#'
+const SPRING_UNKNOWN_C byte = '?'
 
-var TRY_VALUES = []byte{SPRING_GOOD, SPRING_BAD}
+const SPRING_GOOD = string(SPRING_GOOD_C)
+const SPRING_BAD = string(SPRING_BAD_C)
+const SPRING_UNKNOWN = string(SPRING_UNKNOWN_C)
+
+var TRY_VALUES = []string{SPRING_GOOD, SPRING_BAD}
 
 type Record struct {
 	Values   string
@@ -80,22 +93,22 @@ func HasUnknowns(values string) bool {
 // FindSegments returns segments it found up to the first ? and the last character before the ?,
 // or 0 if it reached the end
 func FindSegments(values string) ([]int, byte) {
-	segments := []int{}
+	segments := make([]int, 0, 10)
 	lastChar := byte(0)
 
 	currentSegmentSize := 0
 	for index, r := range values {
 		c := byte(r)
-		if c == SPRING_GOOD {
+		if c == SPRING_GOOD_C {
 			if currentSegmentSize > 0 {
 				segments = append(segments, currentSegmentSize)
 				currentSegmentSize = 0
 			}
-		} else if c == SPRING_BAD {
+		} else if c == SPRING_BAD_C {
 			currentSegmentSize += 1
-		} else if c == SPRING_UNKNOWN {
+		} else if c == SPRING_UNKNOWN_C {
 			if index == 0 {
-				lastChar = SPRING_UNKNOWN
+				lastChar = SPRING_UNKNOWN_C
 			} else {
 				lastChar = byte(values[index-1])
 			}
@@ -108,7 +121,7 @@ func FindSegments(values string) ([]int, byte) {
 	return segments, lastChar
 }
 
-func FindValidConfigs(values string, targetSegments []int) int {
+func FindValidConfigsTOOSLOW(values string, targetSegments []int) int {
 	foundCount := 0
 	//main algo: find the first unknown and try all the values
 	unkInd := strings.Index(values, string(SPRING_UNKNOWN))
@@ -145,14 +158,14 @@ func FindValidConfigs(values string, targetSegments []int) int {
 			//special case for the last segment in the new value
 			//do something different based on the last character
 			if index == len(newSegments)-1 {
-				if lastChar == SPRING_GOOD {
+				if lastChar == SPRING_GOOD_C {
 					//if it was good, the values must be equal or we prune
 					//because we can't make the existing groups bigger
 					if newSegments[index] != targetSegments[index] {
 						slog.Info("Pruning search last seg neq with good")
 						continue
 					}
-				} else if lastChar == SPRING_BAD {
+				} else if lastChar == SPRING_BAD_C {
 					//if it was bad, we only prune if the value is greater than the target value
 					// because equal would be okan and less than might make a bigger group with
 					// more unknowns
@@ -160,7 +173,7 @@ func FindValidConfigs(values string, targetSegments []int) int {
 						slog.Info("Pruning search last seg greater with bad")
 						continue
 					}
-				} else if lastChar == SPRING_UNKNOWN {
+				} else if lastChar == SPRING_UNKNOWN_C {
 					//this happens if the first character is unknown, we just keep going
 				} else {
 					panic("unreachable")
@@ -175,7 +188,7 @@ func FindValidConfigs(values string, targetSegments []int) int {
 		}
 		//if we get here, the newValue is not pruned or halted, so keep going with recursion
 		slog.Info("NOT pruning", "newValues", newValues)
-		newFindCount := FindValidConfigs(newValues, targetSegments)
+		newFindCount := FindValidConfigsTOOSLOW(newValues, targetSegments)
 		foundCount += newFindCount
 	}
 	if foundCount > 0 {
@@ -200,7 +213,7 @@ func (d *Day12) FindAll(findFunc func(string, []int) int) {
 
 func (d *Day12) Part1() {
 	fmt.Println("Part 1")
-	d.FindAll(FindValidConfigs)
+	d.FindAll(FindValidConfigs2)
 }
 
 func (r Record) Expand(multiplier int) Record {
@@ -222,6 +235,107 @@ func (r Record) Expand(multiplier int) Record {
 	}
 }
 
+func TryPeel(values string, segmentSize int) (bool, string) {
+	if len(values) < segmentSize {
+		//can't peel if not enough values
+		return false, values
+	}
+	if strings.Contains(values[:segmentSize], SPRING_GOOD) {
+		//can't peel because one of the starting segmentSize values is good
+		return false, values
+	}
+	if len(values) > segmentSize {
+		//if there is a trailing value, it has to be good value or an unknown
+		if values[segmentSize] == SPRING_GOOD_C || values[segmentSize] == SPRING_UNKNOWN_C {
+			return true, values[segmentSize+1:]
+		} //else we can't peel
+		return false, values
+	} else {
+		//no trailing value to check, so we can peel the remaining values
+		return true, ""
+	}
+}
+
+func TrimGoodAndUnknown(values string) (string, bool) {
+	isChanged := false
+
+	searcher := func(r rune) bool {
+		if byte(r) == SPRING_GOOD_C || byte(r) == SPRING_UNKNOWN_C {
+			isChanged = true
+			return true
+		}
+		return false
+	}
+
+	trimmed := strings.TrimLeftFunc(values, searcher)
+	return trimmed, isChanged
+}
+
+func HasEnoughValues(values string, targetGroups []int) bool {
+	h.Assert(len(targetGroups) > 0, "must have target groups")
+	//check how many values we need
+	minLength := h.SumSlice[int](targetGroups) + (len(targetGroups) - 1)
+	return len(values) >= minLength
+}
+
+var configCache = make(map[string]int, 10000)
+
+func CacheKey(values string, targetGroups []int) string {
+	return fmt.Sprintf("%s-%v", values, targetGroups)
+}
+
+func FindValidConfigs2(values string, targetGroups []int) int {
+	cacheKey := CacheKey(values, targetGroups)
+	{
+		cachedCount, ok := configCache[cacheKey]
+		if ok {
+			return cachedCount
+		}
+	}
+
+	foundCount := 0
+
+	//we can skip any leading good springs
+	values = strings.TrimLeft(values, SPRING_GOOD)
+
+	if !HasEnoughValues(values, targetGroups) {
+		configCache[cacheKey] = foundCount
+		return foundCount
+	}
+
+	{
+		didPeel, remainingValues := TryPeel(values, targetGroups[0])
+		if didPeel {
+			//we can take the first targetSegments[0] values as a group match
+			remainingGroups := targetGroups[1:]
+
+			//halting condition
+			if len(remainingGroups) == 0 {
+				//no more segments, so see if there are any other bad springs
+				//any mix of good and unknown springs represents one solution, otherwise there are none
+				if !strings.Contains(remainingValues, SPRING_BAD) {
+					foundCount += 1
+				} //else no solution found
+			} else {
+				//more segments, so recurse
+				subFoundCount := FindValidConfigs2(remainingValues, remainingGroups)
+				foundCount += subFoundCount
+			}
+		}
+	}
+
+	//regardless of whether we peeled, if there is a leading ?, remove it and recurse
+	if values[0] == SPRING_UNKNOWN_C {
+		//we use the original targetGroups because on this path we have not taken any groups
+		subFoundCount := FindValidConfigs2(values[1:], targetGroups)
+		foundCount += subFoundCount
+	}
+
+	//save the result
+	configCache[cacheKey] = foundCount
+	return foundCount
+}
+
 func (d *Day12) Part2() {
 	fmt.Println("Part 2")
 	records := make([]Record, 0, len(d.records))
@@ -231,5 +345,5 @@ func (d *Day12) Part2() {
 	d.records = records
 	// fmt.Println(d.records)
 
-	d.FindAll(FindValidConfigs)
+	d.FindAll(FindValidConfigs2)
 }
